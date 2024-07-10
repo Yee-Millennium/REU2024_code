d430618a1b5ccf56dcf33c07f4ace2998d9c5f94
David Jiang <logiccubing@gmail.com>
2024-07-09 23:29:50 -0500

    Add new files

--- run_SNLD.py
+++ run_SNLD.py
@@ -11,7 +11,7 @@ def run_SNLD():
     graph_list = []
     for ntwk in ntwk_list:
         ntwk_nonumber = ''.join([i for i in ntwk if not i.isdigit()])
-        path = "data/" + str(ntwk) + '.txt'
+        path = "REU2024_code/data/" + str(ntwk) + '.txt'
         G = nn.NNetwork()
         G.load_add_edges(path, increment_weights=False, use_genfromtxt=True)
         graph_list.append(G)
@@ -20,8 +20,11 @@ def run_SNLD():
 
     # Do sampling, and get the matrix and the feature vector
     A, y = sampling(graph_list, k = 30, sample_size=100)
-
+    print(A,y)
     # Check
+    np.savetxt('Output/A.txt', A, fmt='%d')
+    np.savetxt('Output/y.txt', y, fmt='%d')
+
     print(f"\nThis is the shape of A: {A.shape}")
     print(f"\nThis is the shape of y: {y.shape}")
 
