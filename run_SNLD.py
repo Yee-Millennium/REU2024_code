97c35f27474f96094cac0a19747daa55234f7e70
Yee <yeewei.math@gmail.com>
2024-07-09 19:16:07 -0500

    Update run_SNLD.py

--- run_SNLD.py
+++ run_SNLD.py
@@ -2,23 +2,27 @@ import numpy as np
 from NNetwork import NNetwork as nn
 from src.sampling.sampling import sampling
 
+def run_SNLD():
+    #ntwk_list = ['Caltech36', 'UCLA26', 'MIT8', 'Harvard1']
+    ntwk_list = ['Caltech36', 'UCLA26']
+    sampling_alg = 'pivot'
+    save_folder = 'output/'
 
-#ntwk_list = ['Caltech36', 'UCLA26', 'MIT8', 'Harvard1']
-ntwk_list = ['Caltech36', 'UCLA26']
-sampling_alg = 'pivot'
-save_folder = 'output/'
-
-graph_list = []
-for ntwk in ntwk_list:
-    ntwk_nonumber = ''.join([i for i in ntwk if not i.isdigit()])
-    path = "data/" + str(ntwk) + '.txt'
-    G = nn.NNetwork()
-    G.load_add_edges(path, increment_weights=False, use_genfromtxt=True)
-    graph_list.append(G)
+    graph_list = []
+    for ntwk in ntwk_list:
+        ntwk_nonumber = ''.join([i for i in ntwk if not i.isdigit()])
+        path = "data/" + str(ntwk) + '.txt'
+        G = nn.NNetwork()
+        G.load_add_edges(path, increment_weights=False, use_genfromtxt=True)
+        graph_list.append(G)
 
 
 
-# Do sampling, and get the matrix and the feature vector
-A, y = sampling(graph_list, k = 30, sample_size=100)
-print(f"This is the shape of A: {A}")
-print(f"\nThis is the shape of y: {y}")
+    # Do sampling, and get the matrix and the feature vector
+    A, y = sampling(graph_list, k = 30, sample_size=100)
+    print(f"This is the shape of A: {A}")
+    print(f"\nThis is the shape of y: {y}")
+
+
+if  __name__ == '__main__':
+    run_SNLD()
