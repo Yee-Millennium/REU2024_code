53561dc53ba12e432c5ceaf6fc049a1cae3af591
Yee <yeewei.math@gmail.com>
2024-08-15 03:35:14 -0500

    Modify construction process for labels

--- src/sampling/Sampling.py
+++ src/sampling/Sampling.py
@@ -1,39 +1,45 @@
 import numpy as np 
 from NNetwork import NNetwork as nn
 
-### Return: a matrix of size k^2 * (len(list_graphs)*sample_size),
-### and a vector of size len(list_graphs)*sample_size
+
 def sampling_sndl(list_graphs: list, 
-                  labels = None, 
                   k = 20, 
                   sample_size_list = None, 
                   sample_size = 200,
                   sampling_alg = 'pivot', #RW
                   skip_folded_hom=True
              ):
+    '''
+    args:
+        list_graphs: list of different graphs for sampling
+        k: num of nodes in each sampled subgraph
+        sample_size_list: sample sizes for different graphs in list_graphs
+        sample_size: base sample size when sample_size_list is none
+        sampling_alg: 'pivot'/'RW'
+        skip_folded_hom: reject overlapping sampled k-path
+
+    returns:
+        X_list: numpy array, [num of nodes * num of nodes(subgraph), num of samples]; horizontally concatenated adjacent matrices after vectorization of 
+                sampled subgraphs for supervised network dictionary learning
+        y_list: numpy array, [num of labels-1, num of samples]; matrix of labels (without 0) for supervised network dictionary learning
+    '''
     # list of graphs in NNetwork format
     len_networks = len(list_graphs)
     X_list = []
-    embs_list = []
+    y_list = []
     i = 0
 
     if sample_size_list is None:
         sample_size_list = [sample_size] * len_networks  # Use default sample_size for each graph
 
     # Construct the matrix
-    labels = []
     for idx, G in enumerate(list_graphs):
         sample_size = sample_size_list[idx]
         if sampling_alg != 'RW':
-            for i in np.arange(10):
-                X, embs = G.get_patches(k=k, sample_size=sample_size//10,
-                                        sampling_alg = sampling_alg,
-                                        skip_folded_hom=skip_folded_hom)
-                X_list.append(X)
-                embs_list.append(embs)
-                labels = labels+[idx]*len(embs)
-               # Construct the labels
-            labels = np.asarray(labels)
+            X, embs = G.get_patches(k=k, sample_size=sample_size,
+                                    sampling_alg = sampling_alg,
+                                    skip_folded_hom=skip_folded_hom)
+            X_list.append(X)
             
         else:
             X = []
@@ -44,14 +50,19 @@ def sampling_sndl(list_graphs: list,
                     H = G.k_node_ind_subgraph(k=k)
                 Adj = H.get_adjacency_matrix()
                 X.append(Adj.reshape(1,-1))
-                embs.append(list(H.nodes()))
             X = np.asarray(X)
             X = X[:,0,:].T
-
             X_list.append(X)
-            embs_list.append(embs)
-    
+        
+        # construct matrix of labels
+        real_sample_size = X.shape[1]
+        y = np.zeros(len_networks-1)
+        if idx != 0:
+            y[idx-1] = 1 
+        y_matrix = np.tile(y, (real_sample_size, 1)).T
+        y_list.append(y_matrix)
+
     X_list = np.concatenate(X_list, axis=1)
+    y_list = np.concatenate(y_list,axis=1)
 
-
-    return X_list, labels
+    return X_list, y_list
