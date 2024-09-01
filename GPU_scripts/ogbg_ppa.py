3e50da3447104d1a58ef16bba7beeed18df723b4
Yee <yeewei.math@gmail.com>
2024-09-01 17:19:25 -0500

    Update ogbg_ppa.py

--- GPU_scripts/ogbg_ppa.py
+++ GPU_scripts/ogbg_ppa.py
@@ -3,7 +3,6 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 import torch
 from torch.autograd import Variable
-from util.plotting  import *
 
 
 import sys
@@ -11,15 +10,20 @@ sys.path.append('../src/sampling')
 from Sampling import sampling_graph_classification
 sys.path.append('../src/supervised_NDL')
 from SMF_torch import smf
+sys.path.append('../src/util/plotting')
+from plotting import *
+import pickle
 
 
 dataset_ppa = PygGraphPropPredDataset(name = 'ogbg-ppa')
 
-### Before doing experiments, modify k(hidden_size) and sample_size.
-k = 10
+### Before doing experiments, modify k(hidden_size), sample_size, and num_epochs
+k = 20
+sample_size=5
 
-X , y = sampling_graph_classification(dataset = dataset_ppa, k = k, sample_size=20, 
-                                          has_edge_feature=True)
+X , y = sampling_graph_classification(dataset = dataset_ppa, k = k, sample_size=sample_size, # 1, 5
+                                          has_edge_feature=True, skip_folded_hom=True,
+                                          info_print=False)
 
 Accuracy = []
 test_size = 0.3
@@ -39,12 +43,12 @@ smf_model = smf(X_train, y_train, hidden_size=k, device='cuda')
 results_dict = smf_model.fit(num_epochs=200,
                lr_classification=0.01,
                lr_matrix_factorization=0.01,
-               xi=1,
+               xi=5,
                initialize='spectral',
                W_nonnegativity=True,
                H_nonnegativity=True,
                test_data=[X_test, y_test],
-               record_recons_error=True)
+               record_recons_error=False)
 
 W = results_dict.get('loading')[0]
 beta= results_dict.get('loading')[1]
@@ -54,4 +58,6 @@ display_dict_and_graph(save_path=
                        f'./ogbg_ppa', W=W[:k**2], regression_coeff=beta.T, 
                        fig_size=[15,15], plot_graph_only=True)
 
-np.savetxt("./results_dict", results_dict, fmt='%f')
+### Save the result 
+with open(f"./results_dict_{k}_{sample_size}.pkl", 'wb') as file:
+    pickle.dump(results_dict, file)
